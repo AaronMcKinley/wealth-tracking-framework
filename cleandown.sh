dec91258c05429e2e889a0e3d729f01ded773187
Aaron McKinley <Aaron.McKinley@ibm.com>
2025-08-05 21:21:56 +0100

    updating cleanup script

--- cleandown.sh
+++ cleandown.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Excluding volumes: jenkins_data, postgres_data"
+EXCLUDE_VOLUMES=("jenkins_data" "postgres_data")
+
 echo "Stopping and removing ALL containers..."
 docker ps -aq | xargs -r docker stop
 docker ps -aq | xargs -r docker rm -f
@@ -8,10 +11,17 @@ docker ps -aq | xargs -r docker rm -f
 echo "Removing ALL images..."
 docker images -aq | xargs -r docker rmi -f
 
-echo "Removing ALL unused volumes..."
-docker volume ls -q | xargs -r docker volume rm
+echo "Removing ALL volumes EXCEPT jenkins_data and postgres_data..."
+for volume in $(docker volume ls -q); do
+  if [[ ! " ${EXCLUDE_VOLUMES[@]} " =~ " ${volume} " ]]; then
+    echo "Removing volume: $volume"
+    docker volume rm "$volume"
+  else
+    echo "Skipping volume: $volume"
+  fi
+done
 
 echo "Pruning ALL unused networks..."
 docker network prune -f
 
-echo "Docker clean-down complete!"
+echo "Docker clean-down complete! Jenkins and Postgres volumes preserved."
