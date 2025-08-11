1472b5d30d0d95306fd9f19f9b507016085ecbf1
Aaron McKinley <Aaron.McKinley@ibm.com>
2025-08-12 00:21:05 +0100

    adding cypress reporter

--- wtf-cypress/cypress.config.js
+++ wtf-cypress/cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('cypress');
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'spec, @shelex/cypress-allure-plugin',
+    '@shelex/cypress-allure-plugin': {
+      resultsDir: 'allure-results',
+    },
+  },
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'https://wealth-tracking-framework.com',
     specPattern: 'smoke/**/*.cy.js',
@@ -9,10 +16,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
-    }
+    },
   },
   env: {
     allure: true,
-    allureResultsPath: 'allure-results'
-  }
+    allureResultsPath: 'allure-results',
+  },
 });
