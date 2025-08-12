1a7b73f39e64fed8c82a552f84a7f8cef3c09d79
Aaron McKinley <Aaron.McKinley@ibm.com>
2025-08-12 01:28:51 +0100

    configing cypress folder to default to try get allure working

--- wtf-cypress/cypress.config.js
+++ wtf-cypress/cypress.config.js
@@ -4,8 +4,8 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'https://wealth-tracking-framework.com',
-    specPattern: 'smoke/**/*.cy.js',
-    supportFile: 'support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
@@ -13,6 +13,6 @@ module.exports = defineConfig({
   },
   env: {
     allure: true,
-    allureResultsPath: 'allure-results'
+    allureResultsPath: './allure-results'
   }
 });
--- wtf-cypress/support/e2e.js
+++
@@ -1,5 +0,0 @@
-require('@shelex/cypress-allure-plugin');
-
-before(() => {
-  console.log('[cypress] support/e2e.js loaded');
-});
